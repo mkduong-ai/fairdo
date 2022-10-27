bd6c15c7e86ba936a1aa1d4829559d82e065221f
Manh Khoi Duong <duong@hhu.de>
2022-10-27 16:48:56 +0200

    Refactor

--- src/metrics/_independence.py
+++ src/metrics/_independence.py
@@ -3,14 +3,41 @@ from scipy.stats import rankdata
 from sklearn.metrics import mutual_info_score, normalized_mutual_info_score
 
 
-def mutual_information(y: np.array, z: np.array, bins=2, **kwargs):
+def mutual_information(y: np.array, z: np.array, bins=2, **kwargs) -> float:
+    """
+    Measures whether two variables are independent by calculating their mutual information
+
+    Parameters
+    ----------
+    y: np.array
+    z: np.array
+    bins: int
+    kwargs: keyworded arguments
+
+    Returns
+    -------
+    float
+    """
     # discrimination measurement
     c_zy = np.histogram2d(z, y, bins)[0]
     mi = mutual_info_score(None, None, contingency=c_zy)
     return mi
 
 
-def normalized_mutual_information(y: np.array, z: np.array, **kwargs):
+def normalized_mutual_information(y: np.array, z: np.array, **kwargs) -> float:
+    """
+    Returns the normalized mutual information between two variables.
+
+    Parameters
+    ----------
+    y: np.array
+    z: np.array
+    kwargs: keyworded arguments
+
+    Returns
+    -------
+    float
+    """
     # Normalizes mutual information to 0 (independence) and 1 (perfect correlation)
     return normalized_mutual_info_score(y, z)
 
--- src/preprocessing/__init__.py
+++ src/preprocessing/__init__.py
@@ -1,5 +1,4 @@
 from src.preprocessing._base import *
-from src.preprocessing._helper import *
 from src.preprocessing._metricoptimizer import *
 from src.preprocessing._unawareness import *
 from src.preprocessing._uniform import *
