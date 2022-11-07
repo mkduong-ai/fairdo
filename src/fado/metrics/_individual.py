35b40908b905d54b630a80c0942cb75f230781a4
Manh Khoi Duong <duong@hhu.de>
2022-11-07 18:43:41 +0100

    Remove aif360 dependency

--- src/fado/metrics/_individual.py
+++ src/fado/metrics/_individual.py
@@ -1,19 +1,40 @@
 import numpy as np
-from aif360.sklearn.metrics import consistency_score
+from sklearn.neighbors import NearestNeighbors
 
 
-def consistency_score_objective(x, y, n_neighbors=5, **kwargs):
+def consistency_score(x: np.array, y: np.array, n_neighbors=5, **kwargs) -> float:
+    """
+
+    Parameters
+    ----------
+    x: array-like
+    y: array-like
+    n_neighbors: int
+    kwargs
+
+    Returns
+    -------
+    numeric
+    """
+    nbrs = NearestNeighbors(n_neighbors=n_neighbors, algorithm='ball_tree')
+    nbrs.fit(x)
+    indices = nbrs.kneighbors(x, return_distance=False)
+
+    return 1 - abs(y - y[indices].mean(axis=1)).mean()
+
+
+def consistency_score_objective(x: np.array, y: np.array, n_neighbors=5, **kwargs) -> float:
     """
     Lower score implies more individual fairness
 
     Parameters
     ----------
-    X
-    y
-    n_neighbors
+    x: array-like
+    y: array-like
+    n_neighbors: int
 
     Returns
     -------
-
+    numeric
     """
     return 1 - consistency_score(x, y, n_neighbors)
