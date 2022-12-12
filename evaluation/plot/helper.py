3814e3aaf1bf2488cd6bc55ff24680c8de3c9fe9
Manh Khoi Duong <duong@hhu.de>
2022-12-12 02:38:36 +0100

    Transpose evaluation figures

--- evaluation/plot/helper.py
+++ evaluation/plot/helper.py
@@ -62,7 +62,7 @@ def plot_dataframe_aggregate(results_df: pd.DataFrame,
     yerr = yerr_std
 
     # figsize
-    plt.figure(figsize=(5, 2.5), dpi=80)
+    plt.figure(figsize=(4, 2.2), dpi=80)
     for i in range(len(x_mean)):
         if model is not None:
             label = eval(x_mean.index[i])[1]
@@ -74,7 +74,7 @@ def plot_dataframe_aggregate(results_df: pd.DataFrame,
 
     # title, legend, labels
     # plt.title(f"{dataset.upper()} Dataset")
-    plt.legend(loc='lower right', prop={'size': 8})
+    plt.legend(loc='lower right', prop={'size': 10})
     #plt.xlabel(f"{protected_attribute.capitalize()} Discrimination ({x_axis})")
     plt.xlabel(f"{dataset.capitalize()} ({x_axis})")
     plt.ylabel(y_axis)
