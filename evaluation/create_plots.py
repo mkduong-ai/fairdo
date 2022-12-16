d2f41141773e5fd242a99b35751eead5f0572c1c
Manh Khoi Duong <duong@hhu.de>
2022-12-16 21:35:50 +0100

    Add more figures

--- evaluation/create_plots.py
+++ evaluation/create_plots.py
@@ -70,8 +70,8 @@ def main():
                    'plot_all_dataset_metrics': plot_all_datasets_metrics,
                    'plot_fairness_agnostic': plot_fairness_agnostic}
 
-    pick = 'plot_fairness_agnostic'
-    frac = 0.75
+    pick = 'plot_all_dataset_metrics'
+    frac = 1.25
 
     experiments[pick](frac=frac)
 
