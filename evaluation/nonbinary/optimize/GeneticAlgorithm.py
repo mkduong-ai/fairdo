fe3b7282f9ccbdb80ab0d55e4cc870114f8c9160
Manh Khoi Duong <duong@hhu.de>
2023-04-06 21:56:27 +0200

    Add uniform crossover, kpoint crossover

--- evaluation/nonbinary/optimize/GeneticAlgorithm.py
+++ evaluation/nonbinary/optimize/GeneticAlgorithm.py
@@ -69,15 +69,82 @@ def crossover(parents, offspring_size):
     offspring = np.empty(offspring_size)
     for k in range(offspring_size[0]):
         # the crossover point is a random index between 1 and d-1
-        crossover_point = np.random.randint(1, offspring_size[1]-1)
+        crossover_point = np.random.randint(1, offspring_size[1] - 1)
         # parent selection
         # switch between the two parents randomly at each crossover point to create the offspring
-        parent1_idx = k%parents.shape[0]
-        parent2_idx = (k+1)%parents.shape[0]
+        parent1_idx = k % parents.shape[0]
+        parent2_idx = (k + 1) % parents.shape[0]
         # offspring will have its first half of its genes taken from the first parent.
         offspring[k, 0:crossover_point] = parents[parent1_idx, 0:crossover_point]
         # second half from the second parent.
         offspring[k, crossover_point:] = parents[parent2_idx, crossover_point:]
+    return offspring
+
+
+def uniform_crossover(parents, offspring_size, p=0.5):
+    """
+    Perform the crossover operation with Uniform crossover on the parents to create the offspring
+
+    Parameters
+    ----------
+    parents: numpy array
+        parents of the offspring with shape (2, d)
+    offspring_size: tuple
+        size of the offspring
+    p: float
+        probability of selecting a gene from the first parent, default is 0.5
+
+    Returns
+    -------
+    offspring: numpy array
+    """
+    # perform crossover on the parents to generate new offspring
+    offspring = np.empty(offspring_size)
+    for k in range(offspring_size[0]):
+        # parent selection
+        # switch between the two parents randomly at each gene to create the offspring
+        parent1_idx = k % parents.shape[0]
+        parent2_idx = (k + 1) % parents.shape[0]
+        # create a mask with the same shape as a gene
+        mask = np.random.uniform(size=offspring_size[1]) < p
+        # assign genes to the offspring based on the mask
+        offspring[k] = np.where(mask, parents[parent1_idx], parents[parent2_idx])
+    return offspring
+
+
+def kpoint_crossover(parents, offspring_size, k=2):
+    """
+    Perform the crossover operation with K-point crossover on the parents to create the offspring
+
+    Parameters
+    ----------
+    parents: numpy array
+        parents of the offspring with shape (2, d)
+    offspring_size: tuple
+        size of the offspring
+    k: int
+        number of crossover points, default is 2
+
+    Returns
+    -------
+    offspring: numpy array
+    """
+    # perform crossover on the parents to generate new offspring
+    offspring = np.empty(offspring_size)
+    for i in range(offspring_size[0]):
+        # parent selection
+        # switch between the two parents randomly at each crossover point to create the offspring
+        parent1_idx = i % parents.shape[0]
+        parent2_idx = (i + 1) % parents.shape[0]
+        # calculate the crossover points
+        crossover_points = sorted(np.random.choice(range(1, offspring_size[1]), k-1, replace=False))
+        # alternate between parents in each segment
+        segments = np.array_split(range(offspring_size[1]), len(crossover_points)+1)
+        for j in range(len(segments)):
+            if j % 2 == 0:
+                offspring[i, segments[j]] = parents[parent1_idx, segments[j]]
+            else:
+                offspring[i, segments[j]] = parents[parent2_idx, segments[j]]
     return offspring
 
 
@@ -144,7 +211,7 @@ def genetic_algorithm_constraint(f, d, n, pop_size, num_generations):
         # select the parents
         parents, fitness = select_parents(population, fitness, num_parents=2)
         # create the offspring
-        offspring_size = (pop_size-parents.shape[0], d)
+        offspring_size = (pop_size - parents.shape[0], d)
         offspring = crossover(parents, offspring_size)
         # mutate the offspring
         offspring = mutate(offspring, mutation_rate=0.05)
