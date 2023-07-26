4f8b48d178a4c49233a670d6148b1705944cccc4
Manh Khoi Duong <duong@hhu.de>
2023-07-27 01:54:26 +0200

    remove deprecated function

--- src/fado/utils/helper.py
+++ src/fado/utils/helper.py
@@ -14,25 +14,4 @@ def generate_pairs(lst):
     -------
     list of pairs
     """
-    pairs = []
-    for i in range(len(lst)):
-        for j in range(i + 1, len(lst)):
-            pairs.append((lst[i], lst[j]))
-
-    return pairs
-
-
-def generate_pairs2(lst):
-    """
-    Generate all possible pairs of elements in a list without repetitions
-
-    Parameters
-    ----------
-    lst: list or np.array
-        list of elements
-
-    Returns
-    -------
-    list of pairs
-    """
     return list(combinations(lst, 2))
