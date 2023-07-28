d6e32cc6c6a848f6aaecda62c603eb7ab633a133
Manh Khoi Duong <duong@hhu.de>
2023-07-29 01:20:54 +0200

    Fix consistency score implementation

--- src/fado/metrics/_individual.py
+++ src/fado/metrics/_individual.py
@@ -4,38 +4,62 @@ from sklearn.neighbors import NearestNeighbors
 
 def consistency_score(x: np.array, y: np.array, n_neighbors=5, **kwargs) -> float:
     """
-    Consistency Score in Learning Fair Representations (Zemel et al. 2013)
+    Compute the Consistency Score as defined in Learning Fair Representations (Zemel et al. 2013).
+
+    This score measures the consistency of the output `y` with respect to nearest neighbors in `x`.
+    A higher score indicates more fairness.
 
     Parameters
     ----------
-    x: array-like
-    y: array-like
-    n_neighbors: int
-    kwargs
+    x : np.array
+        Array representing the input data.
+    y : np.array
+        Array of the same length as x, representing the output data.
+    n_neighbors : int, optional
+        Number of neighbors to consider. Default is 5.
+    **kwargs
+        Additional keyword arguments. These are not currently used.
 
     Returns
     -------
-    numeric
+        float
+            The consistency score. Higher values indicate more fairness.
     """
-    nbrs = NearestNeighbors(n_neighbors=n_neighbors, algorithm='ball_tree')
-    nbrs.fit(x)
-    indices = nbrs.kneighbors(x, return_distance=False)
 
-    return 1 - np.mean(np.abs(y - np.mean(y[indices], axis=1)))
+    # fit the KNN model
+    nbrs = NearestNeighbors(n_neighbors=n_neighbors + 1, algorithm='ball_tree').fit(x)
+
+    # get the distances and indices of the k nearest neighbors for each instance
+    distances, indices = nbrs.kneighbors(x)
+
+    # drop the first column (self-loops)
+    indices = indices[:, 1:]
+
+    differences = np.abs(y[indices] - y[:, None])
+
+    return 1 - np.mean(differences)
 
 
 def consistency_score_objective(x: np.array, y: np.array, n_neighbors=5, **kwargs) -> float:
     """
-    Lower score implies more individual fairness
+    Compute the inverse of the Consistency Score to use as an objective function.
+
+    This function is intended to be minimized. Lower values indicate more individual fairness.
 
     Parameters
     ----------
-    x: array-like
-    y: array-like
-    n_neighbors: int
+    x : np.array
+        Array representing the input data.
+    y : np.array
+        Array of the same length as x, representing the output data.
+    n_neighbors : int, optional
+        Number of neighbors to consider. Default is 5.
+    **kwargs
+        Additional keyword arguments. These are not currently used.
 
     Returns
     -------
-    numeric
+    float
+        The inverse of the consistency score. Lower values indicate more fairness.
     """
-    return 1 - consistency_score(x, y, n_neighbors)
+    return 1 - consistency_score(x, y, n_neighbors, **kwargs)
