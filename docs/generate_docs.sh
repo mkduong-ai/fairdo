639151305ba7f20b1f216a6144fdd2bdb3ddf95c
Manh Khoi Duong <duong@hhu.de>
2023-07-29 02:28:51 +0200

    Update generate_docs.sh

--- docs/generate_docs.sh
+++ docs/generate_docs.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+# Find and remove all .rst files in the current directory and subdirectories,
+# except those named 'index.rst'
+find . -name "*.rst" ! -name "index.rst" -type f -exec rm -f {} \;
+
 # Run sphinx-apidoc
 sphinx-apidoc -f -o . ../src/fado/
+
+# Remove older builds if they exist
+if [ -d "_build" ]; then
+    rm -r _build
+fi
 
 # Build the HTML docs
 make html
