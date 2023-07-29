c90593f47eef9f0f2c7074693bb06b102b5d4216
Manh Khoi Duong <duong@hhu.de>
2023-07-29 02:29:06 +0200

    Update sphinx settings

--- docs/conf.py
+++ docs/conf.py
@@ -5,7 +5,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../src/fado'))
+sys.path.insert(0, os.path.abspath('../src/fado/'))
 
 
 # -- Project information -----------------------------------------------------
