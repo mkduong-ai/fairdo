f66b722378ff21e758cda79d77c2e80123c7be18
Manh Khoi Duong <duong@hhu.de>
2023-07-30 03:10:37 +0200

    Add doc string for utils

---
+++ src/fado/utils/__init__.py
@@ -0,0 +1,9 @@
+"""
+Utils Module
+============
+
+The Utils module provides a collection of utility functions that are used across the package.
+
+It includes penalty functions to penalize solutions that do not satisfy the constraint and other
+mathematical functions.
+"""
