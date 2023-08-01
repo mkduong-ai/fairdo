9fe01ebb24f4b410f33bad30665c6c9c9fbfdd7a
Manh Khoi Duong <duong@hhu.de>
2023-08-01 02:35:24 +0200

    Add updated bit_flip_mutation method and new mutation method

--- src/fado/optimize/geneticoperators/mutation.py
+++ src/fado/optimize/geneticoperators/mutation.py
@@ -26,3 +26,47 @@ def mutate(offspring, mutation_rate=0.05):
         # flip the bits
         offspring[idx, mutation_bits] = 1 - offspring[idx, mutation_bits]
     return offspring
+
+
+def bit_flip_mutation(offspring, mutation_rate=0.05):
+    """
+    Mutates the given offspring by flipping each bit with a certain probability.
+
+    Parameters
+    ----------
+    offspring: ndarray, shape (n, d)
+        The offspring to be mutated. Each row represents an offspring, and each column represents a bit.
+    mutation_rate: float, optional
+        The probability of flipping each bit. Default is 0.05.
+
+    Returns
+    -------
+    offspring: ndarray, shape (n, d)
+        The mutated offspring. Each row represents an offspring, and each column represents a bit.
+    """
+    mutation_mask = np.random.rand(*offspring.shape) < mutation_rate
+    offspring[mutation_mask] = 1 - offspring[mutation_mask]
+    return offspring
+
+
+def swap_mutation(offspring):
+    """
+    Mutates the given offspring by randomly selecting two bits and swapping their values.
+
+    Parameters
+    ----------
+    offspring: ndarray, shape (n, d)
+        The offspring to be mutated. Each row represents an offspring, and each column represents a bit.
+
+    Returns
+    -------
+    offspring: ndarray, shape (n, d)
+        The mutated offspring. Each row represents an offspring, and each column represents a bit.
+    """
+    for idx in range(offspring.shape[0]):
+        # select two random bits
+        bit1, bit2 = np.random.choice(np.arange(offspring.shape[1]), 2, replace=False)
+        # swap the bits
+        offspring[idx, bit1], offspring[idx, bit2] = offspring[idx, bit2], offspring[idx, bit1]
+    return offspring
+
