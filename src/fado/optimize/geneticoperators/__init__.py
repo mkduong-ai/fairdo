8bce99ea6323dcf53a9a10c8cbe6cd2df8a6980a
Manh Khoi Duong <duong@hhu.de>
2023-08-02 19:45:50 +0200

    Add description how to use modified genetic operators to GA

--- src/fado/optimize/geneticoperators/__init__.py
+++ src/fado/optimize/geneticoperators/__init__.py
@@ -1,3 +1,27 @@
+"""
+Genetic Operators Module
+========================
+
+The Genetic Operators module provides methods for genetic operators used in the genetic algorithm.
+It is divided into three submodules: `crossover`, `mutation`, and `selection`.
+If the user wants to use different parameter settings for the provided genetic operators,
+they can do so by using Python's `functools.partial` method.
+
+Example
+-------
+>>> from functools import partial
+>>> # Specify your mutation function with some preset parameters
+>>> my_mutation_function = partial(fractional_flip_mutation, mutation_rate=0.10)
+>>> # Define f, d, n, pop_size, num_generations...
+>>> # ...
+>>> # Now you can use my_mutation_function as an argument to genetic_algorithm_constraint
+>>> result = genetic_algorithm_constraint(f, d, n, pop_size, num_generations,
+>>>                                       select_parents=elitist_selection,
+>>>                                       crossover=onepoint_crossover,
+>>>                                       mutate=my_mutation_function, maximize=False)
+
+"""
+
 from fado.optimize.geneticoperators.crossover import *
 from fado.optimize.geneticoperators.mutation import *
 from fado.optimize.geneticoperators.selection import *
