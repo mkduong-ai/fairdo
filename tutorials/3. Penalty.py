c7e33eba49cdaa2d825973a1f1d43be808456dab
Manh Khoi Duong <duong@hhu.de>
2024-02-16 15:46:15 +0100

    Rename

--- tutorials/3. Penalty.py
+++ tutorials/3. Penalty.py
@@ -13,15 +13,15 @@ from fairdo.metrics import group_missing_penalty
 
 # Loading a sample database and encoding for appropriate usage
 # data is a pandas dataframe
-data, label, protected_attributes = load_data('compas')
+data_orig, label, protected_attributes = load_data('compas')
 
 # Create synthetic data
 gc = GaussianCopula()
-gc.fit(data)
-data_syn = gc.sample(data.shape[0])
+gc.fit(data_orig)
+data_syn = gc.sample(data_orig.shape[0])
 
 # Merge/concat original and synthetic data
-data = pd.concat([data, data_syn.copy()], axis=0)
+data = pd.concat([data_orig, data_syn.copy()], axis=0)
 
 # Initial settings for the Genetic Algorithm
 ga = partial(genetic_algorithm,
@@ -42,5 +42,6 @@ data_fair = preprocessor.fit_transform(dataset=data,
                                        penalty_kwargs=penalty_kwargs)
 
 # Print no. samples
+print(data_orig.shape, statistical_parity_abs_diff_max(data_orig[label], data_orig[protected_attributes[0]].to_numpy()))
 print(data.shape, statistical_parity_abs_diff_max(data[label], data[protected_attributes[0]].to_numpy()))
 print(data_fair.shape, statistical_parity_abs_diff_max(data_fair[label], data_fair[protected_attributes[0]].to_numpy()))
